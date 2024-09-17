44b7e07d93db9b1e02d80b68f6bab8a3e73cbf39
natybastazini <natybastazini7@gmail.com>
2024-09-17 09:54:11 -0300

    lotus front-end 17-09

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-
 import Logo from "./public/images/login/logo.svg";
 
 export default function Home() {
@@ -7,50 +6,49 @@ export default function Home() {
     <div className="flex flex-col h-screen">
       <header className="flex flex-row h-[20%] p-4 items-center">
         {/* CABEÇALHO */}
-        <Image src={Logo} alt="logo" className="w-40 h-20"></Image>
-        <div className="w-full h-52 flex items-center justify-center">
-          <h1 className="font-semibold text-4xl text-escrita pt-6">
-            Login
-          </h1>
+        <Image src={Logo} alt="logo" className="w-40 h-20" />
+        <div className="w-full flex flex-col items-center justify-center">
+          <h1 className="font-semibold text-4xl text-escrita">Login</h1>
         </div>
       </header>
-      <main className="grow flex">
+      <main className="flex grow items-center justify-center">
         {/* FORMULÁRIO */}
-        <form className="w-full flex flex-col items-center px-64">
-          <div className="w-full h-full flex flex-col items-center">
-
+        <form className="w-full max-w-md flex flex-col space-y-6 px-4">
+          <div className="flex flex-col">
             {/* E-MAIL */}
-
-            <label class="text-principal text-xl font-semibold py-2" for="email">
+            <label className="text-principal text-xl font-semibold mb-2" htmlFor="email">
               E-mail
             </label>
             <input
               type="email"
-              className="bg-transparent border-2 border-input outline-none w-[50%] h-14 rounded-3xl px-4"
+              id="email"
+              className="bg-transparent border-2 border-input outline-none w-full h-14 rounded-3xl px-4"
               placeholder="E-mail"
-              required="">
-            </input>
-
+              required
+            />
           </div>
-          <div className="w-full h-full flex flex-col items-center  ">
-
+          <div className="flex flex-col">
             {/* PASSWORD */}
-
-            <label class="text-principal text-xl font-semibold py-2" for="password">
+            <label className="text-principal text-xl font-semibold mb-2" htmlFor="password">
               Senha
             </label>
             <input
               type="password"
-              className="bg-transparent border-2 border-input outline-none w-[50%] h-14 rounded-3xl px-4"
+              id="password"
+              className="bg-transparent border-2 border-input outline-none w-full h-14 rounded-3xl px-4"
               placeholder="Senha"
-              required="">
-            </input>
-
+              required
+            />
+          </div>
+          <div className="flex justify-center">
+            <a href="">
+              <button className="w-40 bg-input border-2 border-principal text-principal text-bold rounded-2xl py-2">
+                Entrar
+              </button>
+            </a>
           </div>
         </form>
-
-
       </main>
     </div>
-  );
+  )
 }
